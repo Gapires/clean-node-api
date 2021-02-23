b3a4573d6c0c49c379e4f092d198983f9a401181
Gabriel Pires <gapires.gap@hotmail.com>
2021-02-22 23:53:57 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -16,10 +16,9 @@ module.exports = class LoginRouter {
     if (!password) {
       return HttpResponse.badRequest('password')
     }
+
     this.authUseCase.auth(email, password)
 
-    return {
-      statusCode: 401
-    }
+    return HttpResponse.unauthorizedError()
   }
 }
